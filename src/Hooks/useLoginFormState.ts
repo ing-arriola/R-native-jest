29b59f6af797cd932dd4857645276b81541b3cec
ing-arriola <ingeniero.arriola@gmail.com>
2022-02-26 06:23:53 -0600

    Improved the way to communicate form and hooks

--- src/Hooks/useLoginFormState.ts
+++ src/Hooks/useLoginFormState.ts
@@ -1,17 +1,14 @@
+import { StackNavigationProp } from "@react-navigation/stack";
 import React,{ useState } from "react";
-import { useNavigation } from '@react-navigation/native'
-import { RootStackParamList } from "../../App";
-import { StackNavigationProp } from '@react-navigation/stack';
 
-type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>;
+
 
 export const useLoginFormState = () => {
-  
   
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [submit, setSubmit] = useState(false);
-    const navigation = useNavigation<homeScreenProp>()
+    
   
     let isUsernameValid = false;
     let isPasswordValid = false;
@@ -23,6 +20,8 @@ export const useLoginFormState = () => {
     if (password === "asd") {
       isPasswordValid = true;
     }
+
+    
   
     return {
       username: {
@@ -35,26 +34,10 @@ export const useLoginFormState = () => {
         set: setPassword,
         valid: isPasswordValid,
       },
-      submit: {
-        value: submit,
-        set: () => {
-          setSubmit(true);
-  
-          if (isUsernameValid && isPasswordValid) {
-            fetch("https://jsonplaceholder.typicode.com/users", {
-              method: "POST",
-              body: JSON.stringify({
-                username,
-                password,
-              }),
-            })
-              .then((response) => response.json())
-              .then(() => navigation.navigate("Home"))
-              .catch((error) => {
-                console.log("error", error);
-              });
-          }
-        },
-      },
-    };
-  };
+      submit,
+      setSubmit,
+      isUsernameValid,
+      isPasswordValid
+  }
+
+}
